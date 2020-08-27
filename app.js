e68da267c4f404e312aa58e22e0eb7d15bd41f06
Susmita Mishra <heysusmita@gmail.com>
2020-08-27 14:58:24 +0530

    Add bootstrap and working buttons

--- app.js
+++ app.js
@@ -16,18 +16,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.get("/", function(req, res){
+	res.render("home", {homeStartingContent: homeStartingContent});
+});
 
+app.get("/about", function(req, res){
+	res.render("about", {aboutContent: aboutContent});
+});
+app.get("/contact", function(req, res){
+	res.render("contact", {contactContent: contactContent});
+});
 
-
-
-
-
-
-
-
-
-
-
+app.get("/compose", function(req, res){
+	res.render("compose");
+});
+app.post("/compose", function(req, res){
+	console.log(req.body.postName);
+})
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
